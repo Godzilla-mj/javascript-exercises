fb689035038bda9f0841c7e9d1cf592fef2583d5
Myung <god.godzilla.af@gmail.com>
2019-12-11 21:23:55 -0500

    Completed sumAll

--- sumAll/README.md
+++ sumAll/README.md
@@ -1,3 +1,4 @@
+<!--Completed 12/11/2019 -->
 # Exercise 05 - sumAll
 
 Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+	let sum = 0
+	if (min < 0 || max < 0) return("ERROR");
+	if (typeof min !== "number" || typeof max !== "number") return "ERROR";
+	if (min > max){
+		let newMax = min
+		min = max
+		max = newMax
+	}
 
+	for (i = min; min <= max; max--){
+		sum += max
+	}
+	return sum
 }
 
 module.exports = sumAll
