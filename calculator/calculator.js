99730aadf4e1d400b42b58d782d63c7b45f238c4
Myung <god.godzilla.af@gmail.com>
2020-01-09 20:41:41 -0500

    calculator finished

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,45 @@
-function add () {
-	
+function add (a, b) {
+	return a+b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a-b;
 }
 
-function sum () {
-	
+function sum (numbers) {
+	if (numbers.length < 1) {return 0};
+	let sum = 0
+	return numbers.reduce((number, sum) => sum += number);
 }
 
-function multiply () {
-	
+function multiply (values) {
+	total = 0
+	return values.reduce((value, total) => total *= value);
 }
 
-function power() {
-	
+function power(value, exponent) {
+	return(value ** exponent);
 }
 
-function factorial() {
-	
+function factorial(value) {
+/*	total = 1
+	if(value == 1 || value == 0){return 1};
+	while (value > 1){
+		total *= value
+		value--;
+		
+	}
+	return total;
+*/
+	total = 1
+	if(value == 0 || value == 1){return 1};
+	for(let i = value; i >= 1; i--){
+		total *= i;
+	}
+	return total
+
 }
+
 
 module.exports = {
 	add,
